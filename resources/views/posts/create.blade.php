433f4b2d76b320c668052091f7f5ebf62cded37b
Nataša Minić <nminicn@gmail.com>
2021-06-29 22:53:24 +0200

    Dodavanje create funkcije.

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Create Post</h1>
+
+   <!-- Čuvanje objava u DB
+   Instaliranje Laravel Collective
+   composer require laravelcollective/html -->
+
+
+
+   {!! Form::open(['action' => 'App\Http\Controllers\PostsController@store', 'method' => 'POST']) !!}
+   <div class = "form-group">
+    {{Form::label('title', 'Title')}}
+    {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
+    <br>
+    {{Form::label('body', 'Body')}}
+    {{Form::textarea('title', '', ['class' => 'form-control', 'placeholder' => 'Body'])}}
+
+   </div>
+
+   {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
+    
+    {!! Form::close() !!}
+
+
+@endsection
